6b203fa39b9b988d73cde919e663ee3ef11e352c
Olena Mustafina <toysamiy.mail@gmail.com>
2025-05-13 15:03:07 +0300

    feat: Add SearchBar component with input and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import SearchBar from "./components/SearchBar/SearchBar";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="app">
       <h1>Weather</h1>
+      <SearchBar />
     </div>
   );
 };
