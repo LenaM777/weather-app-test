26686f1cea8aadc6337e42c36953870c240f366d
Olena Mustafina <toysamiy.mail@gmail.com>
2025-05-13 14:56:50 +0300

    feat: Add basic App component structure and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <h1>Weather</h1>
+    </div>
+  );
 };
 
 export default App;
